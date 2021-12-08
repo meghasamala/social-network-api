e920df49042c9cc36eddab95f1a8629225506f07
megha <msamala21@gmail.com>
2021-12-07 18:35:09 -0600

    temporarily remove date getter method

--- models/Thought.js
+++ models/Thought.js
@@ -16,8 +16,7 @@ const ReactionSchema = new Schema({
   },
   createdAt: {
     type: Date,
-    default: Date.now,
-    get: createdAtVal 
+    default: Date.now, 
   },
   },
   {
@@ -36,8 +35,7 @@ const ThoughtSchema = new Schema({
     },
     createdAt: {
       type: Date,
-      default: Date.now,
-      get: createdAtVal 
+      default: Date.now, 
     },
     username: {
       type: String,
